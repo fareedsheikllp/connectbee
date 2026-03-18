7797028e22f7a826e4f08a2ab4665f7155f8abe6
fareedsheikllp <fareedsheikllp@outlook.com>
2026-03-18 12:43:45 -0400

    fixed misplaced consle log

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,9 +1,9 @@
 import { put } from "@vercel/blob";
 import { NextResponse } from "next/server";
 import { auth } from "@/lib/auth";
-console.log("BLOB TOKEN:", process.env.BLOB_READ_WRITE_TOKEN ? "exists" : "missing");
 export async function POST(req) {
   try {
+    console.log("BLOB TOKEN:", process.env.BLOB_READ_WRITE_TOKEN ? "exists" : "missing");
     const session = await auth();
     if (!session?.user) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
 
